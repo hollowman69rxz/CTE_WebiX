4f5aa42e33a855bd4ea2d925ddcf2265419c5130
hollowman69rxz <stuard5@yahoo.co.id>
2021-12-12 22:22:09 +0700

    re-test #3

--- sources/views/data.js
+++ sources/views/data.js
@@ -5,10 +5,24 @@ import {data} from "models/records";
 export default class TopView extends JetView {
     config(){
         return {
-            view:"datatable",
-            autoConfig:true,
-            localId:"body",
-            data:data
+          rows:[
+            /*{view:"datatable", autoConfig:true, localId:"header",
+             data:[
+              { id:"title", header:"Film title", fillspace:true },
+              { id:"year", header:"Released", width:80 },
+              { id:"votes",	header:"Votes", width:100 },
+              { id:"rating", header:"Rating", width:100 },
+              { id:"dummy", header:"Dummy column", width:100, hidden:true }
+               ]},*/{
+          
+            cols:[
+              {view:"datatable",
+              autoConfig:true,
+              localId:"body",
+              data:data}
+
+            ]}
+          ]
             
         };
     }
@@ -19,11 +33,12 @@ export default class TopView extends JetView {
         const context = this.ui({
             view:"contextmenu", localId:"context",
             data:[
-                {value:"Add", icon:"wxi-eye"},
-                {value:"Rename", icon:"wxi-pencil"},
-                {value:"Delete",icon:"wxi-trash"},
-                { $template:"Separator" },
-                {value:"Info",icon:"wxi-trash"}]
+                {value:"Product", icon:"wxi-eye"},
+                {value:"Quantity", icon:"wxi-eye"},
+                {value:"UoM",icon:"wxi-eye"},
+                {value:"Price",icon:"wxi-eye"},
+                {$template:"Separator" },
+                {value:"Info",icon:"wxi-user"}]
         });
         context.attachTo(this.$$("body").getNode());
     }
